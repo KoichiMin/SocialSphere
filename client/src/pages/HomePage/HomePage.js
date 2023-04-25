80d18cb2578c87a36bcdf3e9e6c9895057501001
Koichi <koichidminami@gmail.com>
2023-04-25 12:11:51 -0400

    added layout for HomePage

--- client/src/pages/HomePage/HomePage.js
+++ client/src/pages/HomePage/HomePage.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react"
 import PostModal from "../../components/PostModal/PostModal"
-
+import styled from "styled-components"
 
 const HomePage = () => {
     const [messages, setMessages] = useState([])
@@ -14,31 +14,54 @@ const HomePage = () => {
         })
     },[load])
     return(
-        <div>
-            <div>
+        <Content>
+            <SideBar>
                 <ul>
-                    <li>Find friends</li>
-                    <li>Most Recent</li>
-                    <li>Groups</li>
-                    <li>MarketPlace</li>
-                    <li>Watch</li>
-                    <li>Memories</li>
-                    <li>Saved</li>
+                    <li className="li">Find friends</li>
+                    <li className="li">Most Recent</li>
+                    <li className="li">Groups</li>
+                    <li className="li">MarketPlace</li>
+                    <li className="li">Watch</li>
+                    <li className="li">Memories</li>
+                    <li className="li">Saved</li>
                 </ul>
-            </div>
-            <div>
-                <PostModal setLoad={setLoad} load={load}/>
-            </div>
-            {messages.map((message) =>{
-                return (
-                        <div>
-                            {message.data}
-                        </div>
-                    )
-                })}
-
-        </div>
+            </SideBar>
+            <NewsFeed>
+                <div>
+                    <PostModal setLoad={setLoad} load={load}/>
+                </div>
+                {messages.map((message) =>{
+                    return (
+                            <div>
+                                {message.data}
+                            </div>
+                        )
+                    })}
+            </NewsFeed>
+        
+        </Content>
     )
 }
+
+const Content = styled.div`
+    
+`
+
+
+const SideBar = styled.div`
+    height: 0;
+
+    .li{
+        list-style-type:none;
+    }
+`
+
+const NewsFeed = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    /* margin-left: 35%; */
+`
 
 export default HomePage
