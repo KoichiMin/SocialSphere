b98714b9e0ce3c171f55807a6950749581a657c2
Koichi <koichidminami@gmail.com>
2023-05-31 14:28:48 -0400

    added profile picture  layout for the profile page

--- client/src/pages/ProfilePage/ProfilePage.js
+++ client/src/pages/ProfilePage/ProfilePage.js
@@ -1,20 +1,30 @@
 import { useAuth0 } from "@auth0/auth0-react"
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import styled from "styled-components";
 
 const ProfilePage = () =>{
     const {user} = useAuth0();
+    const [userProfile, setUserProfile] = useState(null)
     useEffect(()=>{
         fetch(`http://localhost:4000/get-user-profile-info/${user.email}`)
         .then((res) => res.json())
         .then((data) =>{
+            setUserProfile(data.userData[0])
             console.log(data.userData[0])
         })
     }, [])
     return(
+    userProfile && 
         <Profile>
-            {user.nickname}
-            {/* <img src={image} alt="Profile"/> */}
+            <div>
+            { <ProfileImage src={userProfile.ProfilePicture} alt="Profile"/>}
+            <button>Update Profile Image</button>
+
+            </div>
+            <div>
+                {userProfile.nickname} 
+                <button>Update Name</button>
+            </div>
         </Profile>
     )
 
@@ -23,6 +33,13 @@ const ProfilePage = () =>{
 
 const Profile = styled.div`
     padding-top: 5%;
+    display: flex;
+    /* flex-direction: column; */
+`
+
+const ProfileImage = styled.img`
+    width: 20vw;
+    height: 30vh;
 `
 
 export default ProfilePage
